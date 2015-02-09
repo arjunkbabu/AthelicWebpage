6f60b51fb35e0f35c84665de8502e8fa463641f9
Arjun_Krishna_Babu <arjunkbabu@users.noreply.github.com>
2015-02-09 23:29:31 +0530

    Improved implementation of registration status

--- rep_gen2.php
+++ rep_gen2.php
@@ -12,16 +12,42 @@ if (!$conn) {
     echo "Connection failed: " . mysqli_connect_error();
 }
 
-$sql = "SELECT  `STUDENT`.`name` ,  `STUDENT`.`roll` ,  `STUDENT`.`batch` 
+$sql_m101 = "SELECT  `STUDENT`.`name` ,  `STUDENT`.`roll` ,  `STUDENT`.`batch` 
 FROM  `STUDENT` WHERE  `STUDENT`.`roll` IN ( SELECT  `REGISTRATION`.`roll` FROM  `REGISTRATION` 
 WHERE  `event_id` = 101) order by `STUDENT`.`batch`";
 
-$result = mysqli_query($conn, $sql);
+$result = mysqli_query($conn, $sql_m101);
 ?>
 <table>
 <?php
 	if (mysqli_num_rows($result) > 0) {
 	    // output data of each row
+	    echo "<br> 100 m MALE <br>";
+	    while($row = mysqli_fetch_assoc($result)) {
+	    	echo "<tr>";
+	    	echo "<td>" . $row["name"] . "</td>" . "<td>". $row["roll"] . "</td>" . "<td>". $row["batch"] . "</td>";
+	    	echo "</tr>";
+	    
+	    //    echo "Name: " . $row["name"]. " - Name: " . $row["roll"]. " " . $row["batch"]. "<br>";
+	    }
+	} else {
+	    echo "0 results";
+	}	
+?>
+</table>
+<?php
+
+$sql_f201 = "SELECT  `STUDENT`.`name` ,  `STUDENT`.`roll` ,  `STUDENT`.`batch` 
+FROM  `STUDENT` WHERE  `STUDENT`.`roll` IN ( SELECT  `REGISTRATION`.`roll` FROM  `REGISTRATION` 
+WHERE  `event_id` = 201) order by `STUDENT`.`batch`";
+
+$result = mysqli_query($conn, $sql_f201);
+?>
+<table>
+<?php
+	if (mysqli_num_rows($result) > 0) {
+	    // output data of each row
+	    echo "<br> 100 m FEMALE <br>";
 	    while($row = mysqli_fetch_assoc($result)) {
 	    	echo "<tr>";
 	    	echo "<td>" . $row["name"] . "</td>" . "<td>". $row["roll"] . "</td>" . "<td>". $row["batch"] . "</td>";
